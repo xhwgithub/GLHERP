d99af994b0d2b112002904c7b7e0fcfe3f06f4b6
“heywei” <1565644290@qq.com>
2025-12-28 10:28:16 +0800

    修改配置文件

--- jshERP-web/build_web_images.sh
+++ jshERP-web/build_web_images.sh
@@ -3,4 +3,10 @@ docker build -t jsh-erp-web:latest .
 
 # 运行容器
 docker run -d -p 80:80 jsh-erp-web:latest
+docker run -d \
+  --name glherp-front \
+  -p 80:80 \
+  -v /app/code/GLHERP/jshERP-web/erp-nginx.conf:/etc/nginx/conf.d/default.conf \
+  -v /app/code/GLHERP/jshERP-web/dist:/usr/share/nginx/html \
+  --platform=linux/amd64 nginx:alpine
 
